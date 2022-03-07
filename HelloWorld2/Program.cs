35ef8e2e0ed8da1bb7058c04cf8eb29b58b8dc6a
esfekia <sertac@gmail.com>
2022-03-07 06:32:11 -0500

    "HelloWorld2.Program written, first code in c#!

---
+++ HelloWorld2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace HelloWorld2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}

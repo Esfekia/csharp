0ee03370470c0215f985efb6f92e73006ded42c7
esfekia <sertac@gmail.com>
2022-03-10 06:23:54 -0500

    Chapter 4.3: Classes - created a public class Person and used it to display the 'john' object on screen.

---
+++ CSharpFundamentals/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFundamentals
+{
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public void Introduce()
+        {
+            Console.WriteLine("My name is " + FirstName + " " + LastName);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+        }
+    }
+}

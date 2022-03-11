71bcaa6f276a5904fc9a255bb41e29ac61a2aea2
esfekia <sertac@gmail.com>
2022-03-11 05:36:52 -0500

    Chapter 3.8 - Strings examples

---
+++ CSharpFundamentals/Program_strings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "Mosh";
+            var lastName = "Hamedani";
+
+            var fullName = firstName + " " + lastName;
+            var myFullName = string.Format("My name is {0}, {1}", firstName, lastName);
+            
+            var names = new string[3] { "John", "Jack", "Mary"};
+            var formattedNames = string.join(",", names);
+            Console.WriteLine(formattedNames);
+
+            
+        }
+    }
+}

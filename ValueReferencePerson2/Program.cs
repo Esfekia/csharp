2c2a57fd36644ccd52224f1cd0a57b88a4dcce39
esfekia <sertac@gmail.com>
2022-03-12 06:37:05 -0500

    Reference type call example added (MakeOld)

--- ValueReferencePerson2/Program.cs
+++ ValueReferencePerson2/Program.cs
@@ -17,6 +17,9 @@ namespace ValueReferencePerson2
             var number = 1;
             Increment(number);
             Console.WriteLine(number);
+            var person = new Person() { Age = 20 };
+            MakeOld(person);
+            Console.WriteLine(person.Age);  
         }
 
         public static void Increment(int number)

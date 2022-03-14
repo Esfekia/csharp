919c2b4607c3af43171d7983a9c8bccd8706d04b
esfekia <sertac@gmail.com>
2022-03-14 06:13:44 -0400

    (cont.) Using switch/case as conditional.

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -6,19 +6,37 @@ using System.Threading.Tasks;
 
 namespace Conditionals
 {
+   
     internal class Program
     {
         static void Main(string[] args)
         {
-           bool isGoldCustomer = false;
-         /*float price;
-           if (isGoldCustomer)
-               price = 19.95f;
-           else
-               price = 29.95f;
-         */
-            float price = (isGoldCustomer) ? 19.95f : 29.95f;
-            Console.WriteLine(price);
+            /* bool isGoldCustomer = false;
+              float price;
+                if (isGoldCustomer)
+                    price = 19.95f;
+                else
+                    price = 29.95f;
+
+                 float price = (isGoldCustomer) ? 19.95f : 29.95f;
+                 Console.WriteLine(price);
+            */
+            var season = Season.Autumn;
+
+            switch (season)
+            {
+                case Season.Autumn:
+                    Console.WriteLine("It's Autumn and a beautiful season.");
+                    break;
+
+                case Season.Summer:
+                    Console.WriteLine("It's Summer and perfect to go to beach.");
+                    break;
+                default:
+                    Console.WriteLine("I do not understand the season.");
+                    break;
+            }
+
         }
     }
 }

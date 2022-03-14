68a5318d4f6bec3b825593279f8358aaba780bca
esfekia <sertac@gmail.com>
2022-03-14 06:27:06 -0400

    Exercise 1: Check if number is between 1 and 10. Print valid if so, invalid if not.

---
+++ Exercise1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*1- Write a program and ask the user to enter a number. 
+ * The number should be between 1 to 10. If the user enters a valid number, display "Valid" on the console. 
+ * Otherwise, display "Invalid". (This logic is used a lot in applications where values entered into input boxes need to be validated.)
+ * */
+namespace Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 12;
+            if (number >0 && number <= 10)
+            {
+                Console.WriteLine("Valid.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid.");
+            }
+
+        }
+    }
+}

2cb9a3ccb2a48e5344280df0d44642228c40b47d
esfekia <sertac@gmail.com>
2022-03-15 05:52:09 -0400

    Conditionals Exercise 4: Speed Camera.

---
+++ Exercise4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Speed Camera
+ * */
+namespace Exercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the speed limit: ");
+            var speedLimit = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter the current speed: ");
+            var currentSpeed = Convert.ToInt32(Console.ReadLine());
+
+            var demerit = Convert.ToInt32((currentSpeed - speedLimit) / 5);
+            if (currentSpeed <= speedLimit)
+            {
+                Console.WriteLine("Ok.");
+            }    
+            else if (demerit <= 12)
+            {
+                Console.WriteLine("You have incurred " + demerit + " points.");
+            }
+            else if (demerit > 12)
+            {
+                Console.WriteLine("License suspended.");
+            }
+
+
+        }
+    }
+}

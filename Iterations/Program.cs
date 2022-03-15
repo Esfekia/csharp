b95999a040771bfe04e84f7078f143f2db053675
esfekia <sertac@gmail.com>
2022-03-15 06:14:30 -0400

    Iteration in a string using for loop.

--- Iterations/Program.cs
+++ Iterations/Program.cs
@@ -3,27 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//Display even numbers between 1-10
+
 namespace Iterations
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            for (var i = 1; i <= 10; i++)
+            //Display even numbers between 1-10
+            /*            for (var i = 1; i <= 10; i++)
+                        {
+                            if (i % 2 == 0)
+                            {
+                                Console.WriteLine(i);
+                            }
+                        }
+            //What if you want to display the numbers from reverse order?
+                        for (var i = 10; i > 0; i--)
+                        {
+                            if ((i % 2) == 0)
+                            {
+                                Console.WriteLine(i);
+                            }
+                        }
+            */
+            var name = "John Smith"; 
+
+            for (var i =0; i < name.Length; i++)
             {
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine(i);
-                }
-            }
-//What if you want to display the numbers from reverse order?
-            for (var i = 10; i > 0; i--)
-            {
-                if ((i % 2) == 0)
-                {
-                    Console.WriteLine(i);
-                }
+                Console.WriteLine(name[i]);
             }
         }
     }

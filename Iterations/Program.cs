5d6c06ea0d2e9b295fe3b3cddc1169d6b30e14fd
esfekia <sertac@gmail.com>
2022-03-15 06:16:15 -0400

    Same iteration done in a string using foreach.

--- Iterations/Program.cs
+++ Iterations/Program.cs
@@ -27,11 +27,16 @@ namespace Iterations
                             }
                         }
             */
-            var name = "John Smith"; 
-
+            var name = "John Smith";
+            /*
             for (var i =0; i < name.Length; i++)
             {
                 Console.WriteLine(name[i]);
+            }
+            */
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
             }
         }
     }

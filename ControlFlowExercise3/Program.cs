4d2480a4c5bd2392f66df6bd985bb4bb1ca13672
esfekia <sertac@gmail.com>
2022-03-17 06:05:16 -0400

    Control Flow Exercise 3: "Write a program and ask the user to enter a number. Compute the factorial of the number and print it on the console."

---
+++ ControlFlowExercise3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+3- Write a program and ask the user to enter a number.
+Compute the factorial of the number and print it on the console.
+For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1
+and display it as 5! = 120.
+*/
+namespace ControlFlowExercise3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number. ");
+            var input = Convert.ToInt32(Console.ReadLine());
+            var factorial = 1;
+            
+            for (var i =1; i <= input; i++)
+                factorial *= i;
+            Console.WriteLine(input + "! = " + factorial);
+        }
+    }
+}

c60b5b60b956dc69948bf2f309c178fe25cbdc62
esfekia <sertac@gmail.com>
2022-03-18 05:57:48 -0400

    Chapter 6.3 - Arrays: Properties and Methods. Example of Length method.

---
+++ ArraysPropertiesMethods/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysPropertiesMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // int[] numbers = new int[] { 3, 7,, 9, 2, 14, 6 };
+            var numbers = new[] { 3, 7, 9, 2, 14, 6 };
+
+            // Length
+            Console.WriteLine("Length: " + numbers.Length);
+        }
+    }
+}

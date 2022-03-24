c3db295587be74fa647eb9676ffe07f188647bbf
esfekia <sertac@gmail.com>
2022-03-24 06:01:25 -0400

    ListArrayExercise5 cont: Displays the smallest 3 numbers in the list entered by user.

--- ListsArraysExercise5/Program.cs
+++ ListsArraysExercise5/Program.cs
@@ -22,12 +22,32 @@ namespace ListsArraysExercise5
                 if (!String.IsNullOrWhiteSpace(input))
                 {
                     elements = input.Split(',');
-                    if (elements.Length <= 5)
+                    if (elements.Length >= 5)
                         break;
                 }
                 Console.WriteLine("Invalid input. Please enter 5 numbers, separated by comma. ");
             }
+            var numbers = new List<int>();
+            foreach (var number in elements)
+                numbers.Add(Convert.ToInt32(number));
 
+            var smallest = new List<int>();
+            while (smallest.Count < 3)
+            {
+                //Assume the first number is the smallest
+                var min = numbers[0];
+                foreach (var number in numbers)
+                {
+                    if (number < min)
+                        min = number;
+                }
+                smallest.Add(min);
+                numbers.Remove(min);
+            }
+
+            Console.WriteLine("The 3 smallest numbers are:");
+            foreach (var number in smallest)
+                Console.WriteLine(number);
 
         }
     }

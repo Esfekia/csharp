e8815480a9311210d53c1f0d9e3a16000d65cab0
esfekia <sertac@gmail.com>
2022-03-25 05:30:35 -0400

    Chapter 7: Working with dates and time, using DateTime module.

---
+++ datetime/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//DateTime System
+namespace datetime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dateTime = new DateTime(2015,1,1);
+            //Could have gotten details all the way to seconds, miliseconds.
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+
+            Console.WriteLine("Hour: " + now.Hour);
+            Console.WriteLine("Minute: " + now.Minute);
+            Console.WriteLine("Second: " + now.Second);
+            //This will give a different output depending on RL datetime!
+        }
+    }
+}

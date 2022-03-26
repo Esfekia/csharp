f39437316a9805a35b3d1c9ad6e4f4e8313bb8f0
esfekia <sertac@gmail.com>
2022-03-26 05:46:23 -0400

    Chapter 8.1 - Working with Text: String Modules. Trim and ToUpper examples.

---
+++ StringMethods/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullName = "Mosh Hamedani ";
+            Console.WriteLine("Trim: '{0}'", fullName.Trim());
+            Console.WriteLine("ToUpper: '{0}'", fullName.ToUpper());
+        }
+    }
+}

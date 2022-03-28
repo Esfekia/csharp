2ef075e508d93d5b399a62d7edfb33fbfd3104b7
Esfekia <sertac@gmail.com>
2022-03-28 13:56:45 -0400

    Chapter 8.4 Summarizing Text Exercise began.

---
+++ SummarizingText/SummarizingText/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummarizingText
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "This is going to be a really really really really really really long text.";
+            const int maxLength = 30;
+            if (sentence.Length < maxLength)
+                Console.WriteLine(sentence);
+            else
+            {
+
+            }
+        }
+    }
+}

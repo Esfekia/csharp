68b8231fc3a3a2a2b3fdf92ae570cc344db8e9f9
esfekia <sertac@gmail.com>
2022-03-29 05:55:56 -0400

    Examples using Insert to insert a new string somwhere inside the string rather than the end.

--- StringBuilderExercise/Program.cs
+++ StringBuilderExercise/Program.cs
@@ -17,8 +17,10 @@ namespace StringBuilderExercise
             builder.AppendLine();
             builder.Append('-', 10);
             builder.Replace('-', '+');
+            builder.Remove(0, 10);
+            builder.Insert(0, new string('-', 10));
             Console.WriteLine(builder);
-            builder.
+           
         }
     }
 }

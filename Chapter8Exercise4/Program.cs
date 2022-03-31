67b699b2f3627f3e1f40e2f32e4238be9e9cba07
esfekia <sertac@gmail.com>
2022-03-31 05:35:25 -0400

    Chapter 8 - Exercise 4: "Use input words to create a variable name with PascalCase" initiated.

---
+++ Chapter8Exercise4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*4- Write a program and ask the user to enter a few words separated by a space. 
+ * Use the words to create a variable name with PascalCase. 
+ * For example, if the user types: "number of students", display "NumberOfStudents". 
+ * Make sure that the program is not dependent on the input. 
+ * So, if the user types "NUMBER OF STUDENTS", the program should still display "NumberOfStudents".*/
+namespace Chapter8Exercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a few words, seperated by a space. ");
+            var input = Console.ReadLine();
+            var names = input.Split(' ');
+            var pascalNames = new StringBuilder();
+            foreach (var word in names)
+            {
+               
+            }
+
+        }
+    }
+}

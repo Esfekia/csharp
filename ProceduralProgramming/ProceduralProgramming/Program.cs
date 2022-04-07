b38e06a17702e6cb20e619e740d6cf3fb7044744
Esfekia <sertac@gmail.com>
2022-04-07 15:41:33 -0400

    Reversing part of the logic has been moved to the ReverseName method, making the code clearer.

--- ProceduralProgramming/ProceduralProgramming/Program.cs
+++ ProceduralProgramming/ProceduralProgramming/Program.cs
@@ -3,22 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+// First Rule: You should always separate the code that works with
+// the console from the code that implements some logic.
 namespace ProceduralProgramming
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            //Main method now only gets input from user and displays it.
             Console.Write("What is your name? ");
             var name = Console.ReadLine();
+            var reversed = ReverseName(name);
+            Console.WriteLine("Reversed name: " + reversed);
+        }
 
+        public static string ReverseName(string name)
+        {
+            //ReverseName method now only responsible for reversing the name.
             var array = new char[name.Length];
             for (var i = name.Length; i > 0; i--)
                 array[name.Length - i] = name[i - 1];
 
             var reversed = new string(array);
-            Console.WriteLine("Reversed name: " + reversed);
+            return reversed;
         }
     }
 }

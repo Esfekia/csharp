f3851d5e33bc1dfebedd466abc6ce5c92d5ab0ae
Esfekia <sertac@gmail.com>
2022-04-07 16:27:48 -0400

    Second Procedural Generation Example - base code prepared.

---
+++ ProceduralGenerationExample2/ProceduralGenerationExample2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProceduralGenerationExample2
+{
+    internal class Program
+    {
+        //Seperate the part about working with console
+        //with the part that works with the logic of the application
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+
+            while (true)
+            {
+                Console.Write("Enter a number (or 'Quit' to exit): ");
+                var input = Console.ReadLine();
+
+                if (input.ToLower() == "quit")
+                    break;
+
+                numbers.Add(Convert.ToInt32(input));
+            }
+
+            var uniques = new List<int>();
+            foreach (var number in numbers)
+            {
+                if (!uniques.Contains(number))
+                    uniques.Add(number);
+            }
+
+            Console.WriteLine("Unique numbers:");
+            foreach (var number in uniques)
+                Console.WriteLine(number);
+        }
+    }
+}

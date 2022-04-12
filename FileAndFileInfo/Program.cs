5c2ae9e430f6607587f9e5deefc6abb3907f8ee1
esfekia <sertac@gmail.com>
2022-04-12 14:56:19 -0400

    Code initiated with double \\ for file paths, before verbatim string trick.

---
+++ FileAndFileInfo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileAndFileInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            File.Copy("c:\\temp\\myfile.jpg", "c:\\temp\\yourfile.jpg", true);
+        }
+    }
+}

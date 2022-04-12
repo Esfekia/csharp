aefacff414d79ddb1d4661c23b53c192d7924606
esfekia <sertac@gmail.com>
2022-04-12 15:09:12 -0400

    Example of reading from a txt file using File.ReadAllText()

--- FileAndFileInfo/Program.cs
+++ FileAndFileInfo/Program.cs
@@ -12,6 +12,7 @@ namespace FileAndFileInfo
         static void Main(string[] args)
         {
             var path = @"c:\temp\yourfile.jpg";
+            var text = @"c:\temp\text.txt";
             File.Copy(@"c:\temp\myfile.jpg", @"c:\temp\yourfile.jpg", true);
             File.Delete(path);
             if (File.Exists(path))
@@ -22,6 +23,8 @@ namespace FileAndFileInfo
             {
                 Console.WriteLine("It got deleted!");
             }
+            Console.WriteLine(File.ReadAllText(text));
+            //File.ReadAllBytes would open a binary file and read all contents of the file into a byte array
         }
     }
 }

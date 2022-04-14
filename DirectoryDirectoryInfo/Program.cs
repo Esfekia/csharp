f5685569321e1f487a476f90870f67d369a160a5
esfekia <sertac@gmail.com>
2022-04-14 11:50:40 -0400

    Using Directory class under System.IO, we created a new folder and we got & displayed contents of another using a foreach loop.

---
+++ DirectoryDirectoryInfo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryDirectoryInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"c:\temp\folder1");
+
+            var files = Directory.GetFiles(@"c:\temp\two", "*.*",SearchOption.AllDirectories);
+            foreach(var file in files)
+                Console.WriteLine(file);
+        }
+    }
+}
